bf365faed27fb42b00caea23f0b2a2ddc0645836
Erik Peteri <peteri.erik@gmail.com>
2023-08-26 10:21:55 +0300

    fixed the xss flaw

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login", views.login, name="login"),
-    path('logout', views.logout, name='logout'),
+    path("login/", views.login, name="login"),
+    path('logout/', views.logout, name='logout'),
     path('watches', views.watches, name="watches"),
     path('details/<int:id>', views.details, name='details'),
     path('watch/<int:id>/handle_description/', views.handle_description, name='handle_description'),
