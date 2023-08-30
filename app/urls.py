b26cb6bf6e2fa95ad66fa94f60f2846751432e32
Erik Peteri <peteri.erik@gmail.com>
2023-08-30 12:50:42 +0300

    refactoring

--- app/urls.py
+++ app/urls.py
@@ -3,12 +3,11 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("login_user", views.login_user, name="login"),
+    path("login_user", views.login_user, name="login_user"),
     path('logout_user', views.logout_user, name='logout'),
     path('watches', views.watches, name="watches"),
     path('details/<int:id>', views.details, name='details'),
     path('watch/<int:id>/handle_description/', views.handle_description, name='handle_description'),
     path('add_watch/', views.add_watch, name='add_watch'),
     path('details/<int:id>/delete_watch/', views.delete_watch, name='delete_watch')
-
 ]
--- app/views.py
+++ app/views.py
@@ -5,27 +5,26 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def home(request):
-    results = []
-    context = {'is_authenticated': request.session.get('username')}
     search_query = request.GET.get('search')
+    context = {'filtered_watches': []}
+
     if search_query:
-       try:
-        filtered_watches = Watch.objects.filter(brand__icontains=search_query)
-        for i in filtered_watches:
-            items = {"id": i.id, "brand": i.brand, "model": i.model}
-            results.append(items)
-        context['filtered_watches'] = results
-       except:
-           error_message = "There was an error."
-           context['error_message'] = error_message
-           return render(request, "home.html", context)
+        if Watch.objects.filter(brand__icontains=search_query).exists():
+            filtered_watches = Watch.objects.filter(brand__icontains=search_query)
+
+            context['filtered_watches'] = [{"id": i.id, "brand": i.brand, "model": i.model} for i in filtered_watches]
+        else:
+            context = {'error_message': 'No results found'}
 
     return render(request, "home.html", context)
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect("home")
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -35,7 +34,7 @@ def login_user(request):
             login(request, user) 
             return redirect('home')
         else:
-            error_message = "There was an error"
+            error_message = "Invalid username or password"
             return render(request, "registration/login.html", {'error_message': error_message})
 
     return render(request, "registration/login.html")
@@ -50,12 +49,12 @@ def watches(request):
     return render(request, "watches.html", {"watches": items})
 
 def details(request, id):
-    item = Watch.objects.get(id=id)
+    item = get_object_or_404(Watch, id=id)
     return render(request, "details.html", {'watch': item})
 
 @login_required
 def handle_description(request, id):
-    item = Watch.objects.get(id=id)
+    item = get_object_or_404(Watch, id=id)
 
     if request.method == 'POST':
         description = request.POST.get('description')
@@ -75,7 +74,7 @@ def add_watch(request):
         brand = request.POST.get('brand')
         model = request.POST.get('model')
 
-        added_by_user = User.objects.get(id=request.user.id)
+        added_by_user = request.user
         Watch.objects.create(brand=brand, model=model, added_by=added_by_user)
 
     return redirect('watches')
@@ -87,6 +86,8 @@ def delete_watch(request, id):
         item.delete()
 
     return redirect('watches')
+
+
 
 
 
